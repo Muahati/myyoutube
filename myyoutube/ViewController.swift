b2e576a56bf362145115dd5ab1c185403c57372a
muahati <muhalati.ahmad@gmail.com>
2020-08-18 23:06:08 +0200

    initial xcode project creation

---
+++ myyoutube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myyoutube
+//
+//  Created by muhalati Ahmad on 18.08.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
